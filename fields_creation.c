460748ce15eef05f3c275ada9e3e7055fed86f53
bahardy <bahardy@uclouvain.be>
2018-05-04 08:36:48 +0200

    Initial commit for fields_creation

---
+++ fields_creation.c
@@ -0,0 +1,98 @@
+//
+// Created by Baptiste Hardy on 4/05/18.
+//
+
+#include "main.h"
+#include "fields_creation.h"
+
+void initialize_fields(Data* data)
+{
+    int m = data->m;
+    int n = data->n;
+    int Np = data->Np;
+    int Ns = data->Ns;
+
+    data->coloring = make2DDoubleArray(m,n);
+    data->CFL_array = make2DDoubleArray(m,n);
+    data->C_n = make3DDoubleArray(Ns,m,n);
+    data->C0 = make1DDoubleArray(Ns); // inlet concentration
+    data->C_n_1 = make3DDoubleArray(Ns,m,n);
+    data->Cs = make3DDoubleArray(Ns,m,n);
+    data->Cp = make2DDoubleArray(Np,Ns);
+    data->dudt = make1DDoubleArray(Np);
+    data->dvdt = make1DDoubleArray(Np);
+    data->domegadt = make1DDoubleArray(Np);
+    data->dTdt = make1DDoubleArray(Np);
+    data->dCdt = make2DDoubleArray(Np,Ns);
+    data->dp = make1DDoubleArray(Np);
+    data->F = make2DDoubleArray(Np,3);
+    data->Fx = make1DDoubleArray(Np);
+    data->Fy = make1DDoubleArray(Np);
+    data->G = make2DDoubleArray(Np,3);
+    data->Ip_S = make3DDoubleArray(Np,m,n);
+    data->Ip_U = make3DDoubleArray(Np,m,n);
+    data->Ip_V = make3DDoubleArray(Np,m,n);
+    data->I_S = make2DDoubleArray(m,n);
+    data->I_U = make2DDoubleArray(m,n);
+    data->I_V = make2DDoubleArray(m,n);
+    data->II = make1DDoubleArray(Np);
+    data->J = make1DDoubleArray(Np);
+    data->Mz = make2DDoubleArray(Np,3);
+    data->omega = make2DDoubleArray(m,n);
+    data->Omega_p = make2DDoubleArray(Np,4);
+    data->phi = make2DDoubleArray(m,n);
+    data->Qm = make2DDoubleArray(Np,Ns);
+    data->P = make2DDoubleArray(m,n);
+    data->PP = make3DDoubleArray(Np, Ns, 3);
+    data->Q = make1DDoubleArray(Np);
+    data->QQ = make2DDoubleArray(Np,3);
+    data->Qr = make2DDoubleArray(Np,3);
+    data->rp = make1DDoubleArray(Np);
+    data->Reh = make2DDoubleArray(m,n);
+    data->Reh_omega = make2DDoubleArray(m,n);
+    data->Sp = make1DDoubleArray(Np);
+    data->theta = make1DDoubleArray(Np);
+
+    data->T_n = make2DDoubleArray(m,n);
+    data->T_n_1 = make2DDoubleArray(m,n);
+    data->Tp = make1DDoubleArray(Np);
+    data->Ts = make2DDoubleArray(m,n);
+    data->Tz = make1DDoubleArray(Np);
+
+    data->u_n = make2DDoubleArray(m,n);
+    data->u_n_1 = make2DDoubleArray(m,n);
+    data->Up = make2DDoubleArray(Np,4);
+    data->u_s = make2DDoubleArray(m,n);
+    data->u_star = make2DDoubleArray(m,n);
+
+    data->v_n = make2DDoubleArray(m,n);
+    data->v_n_1 = make2DDoubleArray(m,n);
+    data->Vp = make2DDoubleArray(Np,4);
+    data->v_s = make2DDoubleArray(m,n);
+    data->v_star = make2DDoubleArray(m,n);
+
+    data->xg = make1DDoubleArray(Np);
+    data->yg = make1DDoubleArray(Np);
+}
+
+void free_fields(Data* data)
+{
+    int m = data->m;
+    int Np = data->Np;
+    int Ns = data->Ns;
+
+    /* Free memory */
+    free(data->xg), free(data->yg), free(data->theta), free(data->dp), free(data->rp), free(data->Sp), free(data->II), free(data->J);
+    free(data->dudt), free(data->dvdt), free(data->domegadt), free(data->dTdt); free2Darray(data->dCdt, Np);
+    free(data->Fx), free(data->Fy), free(data->Tz), free(data->Q), free2Darray(data->Qm, Np);
+    free2Darray(data->u_n,m), free2Darray(data->u_n_1,m), free2Darray(data->u_star,m), free2Darray(data->u_s,m);
+    free2Darray(data->v_n,m), free2Darray(data->v_n_1,m), free2Darray(data->v_star,m), free2Darray(data->v_s,m);
+    free2Darray(data->omega, m); free2Darray(data->Reh,m); free2Darray(data->Reh_omega,m); free2Darray(data->CFL_array, m);
+    free2Darray(data->P,m), free2Darray(data->phi, m);
+    free2Darray(data->T_n,m),  free2Darray(data->T_n_1,m), free2Darray(data->Ts, m);
+    free3Darray(data->C_n, Ns, m), free3Darray(data->C_n_1, Ns, m), free3Darray(data->Cs, Ns, m), free(data->C0);
+    free2Darray(data->Up,Np), free2Darray(data->Vp, Np), free2Darray(data->Omega_p,Np), free(data->Tp), free2Darray(data->Cp, Np);
+    free2Darray(data->F, Np), free2Darray(data->G, Np), free2Darray(data->Mz, Np), free2Darray(data->QQ, Np), free3Darray(data->PP, Np,Ns), free2Darray(data->Qr, Np);
+    free2Darray(data->I_S, m), free2Darray(data->I_U, m), free2Darray(data->I_V, m), free2Darray(data->coloring, m);
+    free3Darray(data->Ip_S, Np,m), free3Darray(data->Ip_U, Np,m), free3Darray(data->Ip_V, Np, m);
+}
---
+++ fields_creation.h
@@ -0,0 +1,11 @@
+//
+// Created by Baptiste Hardy on 4/05/18.
+//
+
+#ifndef INC_2DINCOMP_FIELDS_CREATION_H
+#define INC_2DINCOMP_FIELDS_CREATION_H
+
+void initialize_fields(Data* data);
+void free_fields(Data* data);
+
+#endif //INC_2DINCOMP_FIELDS_CREATION_H
