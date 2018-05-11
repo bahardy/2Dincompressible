cfb58af5c79ed8f5807f64eca9500679fe65b8ab
bahardy <bahardy@uclouvain.be>
2018-05-11 15:42:46 +0200

    Macro definition in poisson solver ok

--- poisson.h
+++ poisson.h
@@ -2,10 +2,10 @@
 // Created by Baptiste Hardy on 9/03/18.
 //
 #include "main.h"
-#ifndef 2DINCOMP_POISSON
-#define 2DINCOMP_POISSON
+#ifndef INCOMP_POISSON_H
+#define INCOMP_POISSON_H
 
 void old_poisson_solver(Data* data);
 PetscErrorCode poisson_solver(Data* data, int myrank, int nbproc);
 
-#endif //2DINCOMP_POISSON
+#endif //INCOMP_POISSON_H
