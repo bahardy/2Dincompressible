3459dacecf201a788c9fa36799c4b9bd7dbc9a2e
bahardy <bahardy@uclouvain.be>
2018-06-14 16:51:12 +0200

    add computation of poisson equation residuals (signature)

--- poisson.h
+++ poisson.h
@@ -9,6 +9,7 @@ void old_poisson_solver(Data* data);
 void old_poisson_solver_periodic(Data* data);
 PetscErrorCode poisson_solver(Data* data, int myrank, int nbproc);
 PetscErrorCode poisson_solver_periodic(Data* data, int myrank, int nbproc);
-
+void poisson_residual(Data* data);
+void poisson_residual_periodic(Data* data);
 
 #endif //INCOMP_POISSON_H
