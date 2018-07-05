e2b9059b09a321e3a6a35e3ee3c0eb823bc5a135
bahardy <bahardy@uclouvain.be>
2018-07-05 11:09:12 +0200

    add set-up in a separate file

---
+++ set_up.h
@@ -0,0 +1,13 @@
+//
+// Created by Baptiste Hardy on 4/07/18.
+//
+
+#ifndef INC_2DINCOMP_SET_UP_H
+#define INC_2DINCOMP_SET_UP_H
+
+#include "main.h"
+
+void set_up(Data* data, int argc, char *argv[], int rank);
+void initialize_fields(Data* data);
+
+#endif //INC_2DINCOMP_SET_UP_H
